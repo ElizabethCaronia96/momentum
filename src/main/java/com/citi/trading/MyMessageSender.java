13110e52c06c6c698b0c9149f4e61b9b158b09ef
Elizabeth Caronia <elizabeth.caronia@rutgers.edu>
2018-07-27 14:54:29 -0400

    "initial project commit"

--- src/main/java/com/citi/trading/MyMessageSender.java
+++ src/main/java/com/citi/trading/MyMessageSender.java
@@ -1,5 +1,9 @@
 package com.citi.trading;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
 @Service
 public class MyMessageSender {
     @Autowired
