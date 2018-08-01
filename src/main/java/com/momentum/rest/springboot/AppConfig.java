2b1e2ab4bb03cc7c2ebaa2e636c02ec370178925
dchen <chendavid96@gmail.com>
2018-08-01 12:13:46 -0400

    testing schedulers in springboot folder

--- src/main/java/com/momentum/rest/springboot/AppConfig.java
+++ src/main/java/com/momentum/rest/springboot/AppConfig.java
@@ -4,15 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableAutoConfiguration
 @ComponentScan
 @EntityScan("com.momentum.rest.entities")
+@EnableScheduling
 public class AppConfig {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(AppConfig.class, args);
-
 
     }
 
---
+++ src/main/java/com/momentum/rest/springboot/FeedGrabber.java
@@ -0,0 +1,71 @@
+package com.momentum.rest.springboot;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class FeedGrabber {
+
+    public static void main(String[] args) throws IOException { //test
+        FeedGrabber f = new FeedGrabber();
+        f.grabFeedPrices();
+    }
+
+    @Scheduled(fixedRate = 1000) // this is in milliseconds
+    public void grabFeedPrices() throws IOException {
+
+        System.out.println("i am running every 1 second");
+
+//
+//        // hard coded list of stocks to check -- use the DB to grab stocks we need to check
+//        String[] stocksToCheck = {"goog", "aapl", "msft", "random", "nsc", "brk-a"};
+//
+//        String requestURL = "http://feed.conygre.com:8080/MockYahoo/quotes.csv?s=%s&f=p0";
+//        String joinedStocksString = String.join(",", stocksToCheck);
+//        String finalrequestURL = String.format(requestURL, joinedStocksString);
+//
+//        Date dt = new java.util.Date();
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//
+//        String currentTime = sdf.format(dt);
+//
+//        System.out.println("i call the feed every second.");
+//
+//        List<Double> priceResults = makeFeedPricesRequest(finalrequestURL);
+//
+//        for (int i = 0; i < stocksToCheck.length; i++) {
+//            // add this to the database
+//
+//            System.out.println(stocksToCheck[i] + "\t" + priceResults.get(i));
+//        }
+
+    }
+
+    private static List<Double> makeFeedPricesRequest(String requestURL) throws IOException {
+        List<Double> results = new ArrayList<>();
+        URL url = new URL(requestURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            results.add(Double.parseDouble(line));
+        }
+        rd.close();
+
+        return results;
+    }
+
+}
