0719e529a841f08323a46c50c527377071efebb9
dchen <chendavid96@gmail.com>
2018-08-01 21:05:27 -0400

    tried new charting libraries for frontend

--- src/main/resources/static/js/momentum.js
+++ src/main/resources/static/js/momentum.js
@@ -1,14 +1,190 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+$(document).ready(function() {
+    smoothScroll();
+    testAMChart();
 });
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-});
+function smoothScroll() {
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+}
 
+function testAMChart() {
+    var chart = AmCharts.makeChart("chartdiv", {
+        "type": "serial",
+        "theme": "dark",
+        "marginRight": 80,
+        "marginTop": 17,
+        "autoMarginOffset": 20,
+        "dataProvider": [{
+            "date": "2012-03-01",
+            "price": 20
+        }, {
+            "date": "2012-03-02",
+            "price": 75
+        }, {
+            "date": "2012-03-03",
+            "price": 15
+        }, {
+            "date": "2012-03-04",
+            "price": 75
+        }, {
+            "date": "2012-03-05",
+            "price": 158
+        }, {
+            "date": "2012-03-06",
+            "price": 57
+        }, {
+            "date": "2012-03-07",
+            "price": 107
+        }, {
+            "date": "2012-03-08",
+            "price": 89
+        }, {
+            "date": "2012-03-09",
+            "price": 75
+        }, {
+            "date": "2012-03-10",
+            "price": 132
+        }, {
+            "date": "2012-03-11",
+            "price": 158
+        }, {
+            "date": "2012-03-12",
+            "price": 56
+        }, {
+            "date": "2012-03-13",
+            "price": 169
+        }, {
+            "date": "2012-03-14",
+            "price": 24
+        }, {
+            "date": "2012-03-15",
+            "price": 147
+        }],
+        "valueAxes": [{
+            "logarithmic": true,
+            "dashLength": 1,
+            "guides": [{
+                "dashLength": 6,
+                "inside": true,
+                "label": "average",
+                "lineAlpha": 1,
+                "value": 90.4
+            }],
+            "position": "left"
+        }],
+        "graphs": [{
+            "bullet": "round",
+            "id": "g1",
+            "bulletBorderAlpha": 1,
+            "bulletColor": "#FFFFFF",
+            "bulletSize": 7,
+            "lineThickness": 2,
+            "title": "Price",
+            "type": "smoothedLine",
+            "useLineColorForBulletBorder": true,
+            "valueField": "price"
+        }],
+        "chartScrollbar": {},
+        "chartCursor": {
+            "valueLineEnabled": true,
+            "valueLineBalloonEnabled": true,
+            "valueLineAlpha": 0.5,
+            "fullWidth": true,
+            "cursorAlpha": 0.05
+        },
+        "dataDateFormat": "YYYY-MM-DD",
+        "categoryField": "date",
+        "categoryAxis": {
+            "parseDates": true
+        },
+        "export": {
+            "enabled": true
+        }
+    });
+
+    chart.addListener("dataUpdated", zoomChart);
+
+    function zoomChart() {
+        chart.zoomToDates(new Date(2012, 2, 2), new Date(2012, 2, 10));
+    }
+}
+
+function testChart(){
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor: 'white',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: 'white'
+                }
+            },
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        color: 'white',
+                        display: false
+                    },
+                    ticks: {
+                        fontColor: "white",
+                        beginAtZero:true
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        color: 'white',
+                        display: false
+                    },
+                    ticks: {
+                        fontColor: "white",
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
